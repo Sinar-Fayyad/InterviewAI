af89a6414c6515e3f4763ee6a4fa59ce0fcd694c
Sino57 <sinarfayyad5@gmail.com>
2026-04-11 14:14:48 +0300

    .

--- Backend/app/Services/ChatbotService.php
+++ Backend/app/Services/ChatbotService.php
@@ -4,14 +4,11 @@ namespace App\Services;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Storage;
 
 class ChatbotService
 {
     static function initializeMemory($user_id)
     {
-        $user_context = []; // Default to an empty array if no user ID is provided (for guest users)
-
         if ($user_id) {
 
             if (!User::find($user_id)) {
@@ -19,7 +16,16 @@ class ChatbotService
             }
             $user_context = UserContextService::build($user_id);
 
+        }else {
+            $user_context = [
+                'user_info'=> [],
+                'education' => [],
+                'experience' => [],
+                'skills' => []
+            ]; // Default to an empty array if no user ID is provided (for guest users)
         }
+
+        $user_context_json = json_encode($user_context);
         $collection_name = "chat_" . ($user_id ?? 'guest') . "_" . uniqid();
 
         $filePath = storage_path('app/private/chatbot/website_guide.md');
@@ -28,34 +34,31 @@ class ChatbotService
             throw new \Exception('Guidance file not found', 404);
         }
 
+        // $response = Http::attach('website_guide', file_get_contents($filePath), basename($filePath))
+        //                 ->timeout(120)->post('http://127.0.0.1:5678/webhook/init_memory', [
+        //             'user_context' => $user_context_json,
+        //             'collection_name' => $collection_name,
+        //         ]);
 
-        $response = Http::withHeaders([
-            'X-N8N-KEY' => config('services.n8n.auth_key'),
-        ])
-            ->attach('website_guide', file_get_contents($filePath), basename($filePath))
-            ->timeout(120)->post('http://127.0.0.1:5678/webhook/init_memory', [
-                    'user_context' => $user_context,
-                    'collection_name' => $collection_name,
-                ]);
+        // if (!$response->successful()) {
+        //     throw new \Exception($response->body(), $response->getStatusCode());
+        // }
 
-        if (!$response->successful()) {
-            throw new \Exception($response->body(), $response->getStatusCode());
-        }
+        // return [
+        //     'is_guest' => !$user_id ? true : false,
+        //     'user_id' => $user_id,
+        //     'status' => 'initialized',
+        //     'collection_name' => $collection_name,
+        //     'collection_id' => $response->json()['collection_id'],
+        // ];
 
-        return [
-            'is_guest' => !$user_id ? true : false,
-            'user_id' => $user_id,
-            'status' => 'initialized',
-            'collection_name' => $collection_name,
-            'collection_id' => $response->json()['collection_id'],
-        ];
+        return $user_context_json;
     }
 
     static function clearMemory($collection_name)
     {
-        $response = Http::withHeaders([
-            'X-N8N-KEY' => config('services.n8n.auth_key'),
-        ])->timeout(120)->post('http://127.0.0.1:5678/webhook/Delete_memory', [
+        $response = Http::timeout(120)
+                          ->post('http://127.0.0.1:5678/webhook/Delete_memory', [
                     'collection_name' => $collection_name,
                 ]);
         if (!$response->successful()) {
