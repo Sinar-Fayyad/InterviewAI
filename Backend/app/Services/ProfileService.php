57cafa33f5533391d5bf45809a350febfd8f09e6
Sino57 <sinarfayyad5@gmail.com>
2026-04-16 12:41:25 +0300

    correct backend

--- Backend/app/Services/ProfileService.php
+++ Backend/app/Services/ProfileService.php
@@ -34,10 +34,10 @@ class ProfileService
 
         UserService::updateUser($request->input('user_info.0'), $user_id);
 
-        collect($request['education'])->each(fn($edu) => EducationService::addEducation( $edu, $user_id,));
-        collect($request['experience'])->each(fn($exp) => ExperienceService::addExperience( $exp, $user_id,));
-        collect($request['skills'])->each(fn($skill) => SkillService::addSkill( $skill ,$user_id));
-        collect($request['certifications'])->each(fn($cert) => CertificationService::addCertification( $cert, $user_id));
+        collect($request['education'])->each(fn($edu) => EducationService::addEducation($edu, $user_id));
+        collect($request['experience'])->each(fn($exp) => ExperienceService::addExperience($exp, $user_id));
+        collect($request['skills'])->each(fn($skill) => SkillService::addSkill($skill, $user_id));
+        collect($request['certifications'])->each(fn($cert) => CertificationService::addCertification($cert, $user_id));
 
     }
 }
