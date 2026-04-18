d21f44c6e07204d9daee02beb3ce41be71468be1
Mirna Al Orm <mirna.alorm@gmail.com>
2026-04-19 01:37:15 +0300

    Update UserService.php

--- Backend/app/Services/UserService.php
+++ Backend/app/Services/UserService.php
@@ -12,9 +12,8 @@ class UserService
             throw new \Exception("User not found", 404);
         }
 
-        return $user ->select('id', 'first_name', 'last_name', 'email', 'phone', 'location', 'summary', 'theme')
-                     ->get()
-                     ->toArray();
+        $userData = $user->only(['id', 'first_name', 'last_name', 'email', 'phone', 'location', 'summary', 'theme']);
+        return [$userData];
     }
 
     static function updateUser($data, $id)
@@ -24,11 +23,11 @@ class UserService
             throw new \Exception("User not found", 404);
         }
 
-        $user->first_name = $data["first_name"]? $data["first_name"]:$user->first_name;
-        $user->last_name = $data["last_name"]? $data["last_name"]:$user->last_name;
-        $user->phone = $data["phone"]? $data["phone"]:$user->phone;
-        $user->location = $data["location"]? $data["location"]:$user->location;
-        $user->summary = $data["summary"]? $data["summary"]:$user->summary;
+        $user->first_name = $data["first_name"] ?? $user->first_name;
+        $user->last_name = $data["last_name"] ?? $user->last_name;
+        $user->phone = $data["phone"] ?? $user->phone;
+        $user->location = $data["location"] ?? $user->location;
+        $user->summary = $data["summary"] ?? $user->summary;
 
         $user->save();
     }
